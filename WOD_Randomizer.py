82cd324dbcffc6f4829d11d81e155ff23ef0f2ba
Jason P Burns <80472387+JasonBurnsInfosec@users.noreply.github.com>
2022-08-15 10:07:33 -0400

    Update WOD_Randomizer.py

--- WOD_Randomizer.py
+++ WOD_Randomizer.py
@@ -1,5 +1,6 @@
 #This is a program to pull a random workout of the day
 #Requires reqeusts and bs4 to be installed. Install with "python3 -m pip install <requests/bs4>"
+#This writes to a local index.html file that apache is presenting. You can modify to have python host if you want.
 import random
 import requests
 from bs4 import BeautifulSoup
@@ -15,14 +16,15 @@ website = "https://crossfit.com/"+wod #actual WOD page
 page_response = requests.get(website, headers={'User-Agent': 'Mozilla/5.0'}, timeout=20) #pulls the random WOD webpage in full
 page_content = BeautifulSoup(page_response.content, "html.parser") #uses beautifulsoup to parse the webpage
 page_wod = str(page_content.find_all("div", {"class": "_wrapper_1uw7e_18 _text-block_1x7b4_17"})) #cuts webpage down to just the WOD section
+page_wod_clean = page_wod.lstrip("[").rstrip("]")
 
 #save WOD to local html for display
 html_clock = '''<!DOCTYPE html>
 <html lang="en" >
-<class="wf-ingracondensed-n8-active wf-ingracondensed-n5-active wf-ingracondensed-n4-active wf-ingracondensed-n3-active wf-ingracondensed-n7-active wf-active"><head><meta http-equiv="Content-type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""><link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet"><link rel="stylesheet" href="https://use.typekit.net/jas7dha.css">
+<class="wf-ingracondensed-n8-active wf-ingracondensed-n5-active wf-ingracondensed-n4-active wf-ingracondensed-n3-active wf-ingracondensed-n7-active wf-active"><head><meta http-equiv="Content-type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""><link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet"><link rel="stylesheet" href="style.css">
 <body onload="startTime()">
 
-<h2>TIMER</h2>
+<h2>WOD TIMER</h2>
 
 <div id="txt"></div>
 
@@ -46,8 +48,8 @@ function checkTime(i) {
 
 </body>'''
 
-local_html = open("/opt/homebrew/var/www/index.html", "w") #open the local html file to write the WOD to, you may need to update this on your system
-local_html.write(html_clock + '\n' + page_wod) #write output to local html file
+local_html = open("/opt/homebrew/var/www/index.html", "w") #open the local html file to write the WOD to, you likely need to change this on your machine
+local_html.write(html_clock + '\n' + page_wod_clean) #write output to local html file
 local_html.close #close local html file
 
 #you can set up a raspberry pi with a monitor to query/display the webpage in your gym to display the WOD now
